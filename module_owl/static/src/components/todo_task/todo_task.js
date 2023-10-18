f6be0829e093a77991a51c7c135de85d93e3c9e0
Mountain Tran <tranngocson8296@gmail.com>
2023-10-18 21:36:30 +0700

    add action

--- module_owl/static/src/components/todo_task/todo_task.js
+++ module_owl/static/src/components/todo_task/todo_task.js
@@ -1,22 +1,70 @@
 /** @odoo-module **/
 
 import { registry } from '@web/core/registry';
-const { Component, useState, onWillStart } = owl;
+const { Component, useState, onWillStart, useRef } = owl;
 import { useService } from "@web/core/utils/hooks";
 
 export class TodoTask extends Component {
     setup() {
         this.state = useState({
-            taskList: [],
+            task:{name:"", color:"#000000", description:"", is_done:false},
+            taskList:[],
+            isEdit: false,
+            activeId: false,
         });
         this.orm = useService("orm")
+        this.model = "todo.task"
+        this.searchInput = useRef("search-input")
         onWillStart(async ()=>{
             await this.getAllTasks()
         })
     }
 
     async getAllTasks(){
-        this.state.taskList = await this.orm.searchRead('todo.task', [], ["name", "color", "is_done", "description"])
+        this.state.taskList = await this.orm.searchRead(this.model, [], ["name", "color", "is_done", "description"])
+    }
+
+    addTask(){
+        this.resetForm()
+        this.state.activeId = false
+        this.state.isEdit = false
+    }
+    
+    editTask(task){
+        this.state.activeId = task.id
+        this.state.isEdit = true
+        this.state.task = {...task}
+    }
+
+    resetForm(){
+        this.state.task = {name:"", color:"#000000", description:"", is_done:false}
+    }
+
+    async saveTask(){
+
+        if (!this.state.isEdit){
+            await this.orm.create(this.model, [this.state.task])
+            this.resetForm()
+        } else {
+            await this.orm.write(this.model, [this.state.activeId], this.state.task)
+        }
+
+        await this.getAllTasks()
+    }
+
+    async deleteTask(task){
+        await this.orm.unlink(this.model, [task.id])
+        await this.getAllTasks()
+    }
+
+    async searchTasks(){
+        const text = this.searchInput.el.value
+        this.state.taskList = await this.orm.searchRead(this.model, [['name','ilike',text]], ["name", "color", "is_done", "description"])
+    }
+
+    async updateColor(e, task){
+        await this.orm.write(this.model, [task.id], {color: e.target.value})
+        await this.getAllTasks()
     }
 
 }
